16734260997e8bb19c05ec105dcdc88493c8af40
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-12-10 20:05:37 -0800

    refactored test to match callbacks

--- testing.js
+++ testing.js
@@ -3,7 +3,6 @@ var downloader = require('./index');
 var url = 'https://docs.google.com/presentation/d/1HsdP94BUaT2xCV3vjvu-3O9fiFfnTrNyKYRQTBUWnYY/embed';
 var url2 = 'https://docs.google.com/presentation/d/16M1roku_Z49q5loX4UVwA4p2soJHE1zasfS_yccP4HA/embed';
 
-downloader.getSVGs(url2)
-.then(function (svgs) {
-  console.log(svgs);
+downloader.getSVGs(url2, function (err, res) {
+  console.log(res);
 });
